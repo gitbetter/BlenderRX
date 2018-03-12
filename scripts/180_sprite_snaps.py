eae9f2439fa8b8524685cadbe8f64a39077c0c11
Adrian <skatehumor@yahoo.com>
2018-03-12 18:11:13 -0400

    Moved target directory into variable

--- scripts/180_sprite_snaps.py
+++ scripts/180_sprite_snaps.py
@@ -4,11 +4,13 @@ from math import radians
 step_count = 13
 selected = bpy.context.selected_objects[0]
 
+target_dir = 'C:\\Users\\skate\\Desktop\\'
+
 for step in range(0, step_count):
     for obj in bpy.data.objects:
         if obj.type != "CAMERA" and obj.type != "LAMP":
             obj.rotation_euler[2] = radians(step * (180.0 / step_count))
 
     bpy.data.scenes["Scene"].cycles.film_transparent = True
-    bpy.data.scenes["Scene"].render.filepath = 'C:\\Users\\skate\\Desktop\\%s%d.jpg' % (selected.name, step)
+    bpy.data.scenes["Scene"].render.filepath = '%s%s%d.jpg' % (target_dir, selected.name, step)
     bpy.ops.render.render( write_still=True )
